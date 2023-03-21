2617253ec8f1991b65b6d492a99d22b0df68ccd7
Daniela Zedníčková <76813964+DannyZednickova@users.noreply.github.com>
2023-03-21 08:48:19 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html class="background-main">
+<head>
+    <title>
+        Muj domaci ukol
+    </title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/styl.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+</head>
+
+<body>
+<div class="card text-center background-main">
+    <div class="card-header ">
+        <ul class="nav nav-tabs card-header-tabs ">
+            <li class="nav-item background-main ">
+                <a class="nav-link" href="#">O nás</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Kalendář akcí </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"> Fotogalerie </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"> Kde nás najdete </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">MERCH</a>
+            </li>
+        </ul>
+    </div>
+    <div class="row">
+        <div class="col-6 move-top-col-6-up">
+            <div class="alert alert-danger alert-transtion" role="alert">
+
+                <h2> NÁSLEDUJÍCÍ AKCE </h2>
+                <p>
+                    Další HOLKOFF akce už tyto VELIKONOCE!
+                </p>
+            </div>
+        </div>
+        <div class="col-6 move-top-col-6-up">
+            <img class="logo-img" src="img/logo.png" alt="">
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-4">
+                    <div class="card" style="width: 100%;">
+                        <img src="img/dirt-bike-g31512cd73_640.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card" style="width: 100%;">
+                        <img src="img/extreme-gc0432cc4e_640.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 ">
+                    <div class="card" style="width: 100%;">
+                        <img src="img/enduro-g74c77a786_640.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
