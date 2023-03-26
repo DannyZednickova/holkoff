0d0a3fb2d64496bac479dbd870e6dc4aa0939f43
Daniela Zedníčková <76813964+DannyZednickova@users.noreply.github.com>
2023-03-26 19:48:34 +0200

    1

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 <html class="background-main">
 <head>
     <title>
-        Muj domaci ukol
-    </title>
+        Muj domaci ukol </title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/styl.css">
     <link rel="stylesheet" href="assets/icons.css">
@@ -12,6 +11,7 @@
 </head>
 
 <body>
+
 <div class="card text-center background-main">
     <div class="card-header ">
         <ul class="nav nav-tabs card-header-tabs ">
@@ -43,107 +43,49 @@
         <div class="col-6 move-top-col-6-up">
             <img class="logo-img" src="img/holkoff-nobackgr.png" alt="">
         </div>
-        <div class="backround-row">
-            <div class="content">
-                <div class="card mb-3 card-header">
-                    <div class="row g-0 uni-justify-align">
-                        <div class="col-md-4">
-                            <img src="img/dirt-bike-g31512cd73_640.jpg" class="top-card-img" alt="...">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body top-card-text">
-                                <div class="high-text" style=" transform: translate(-75px, -2px);">  Počet holek: 300</div>
-                                <div class="high-text" style=" transform: translate(-23px, -2px);">   Počet akcí : 4 </div>
-                                <div class="high-text" style="transform: translate(64px, -2px); "> Počet zážitků: milión</div>
-                            </div>
-                        </div>
+
+        <div class="content smaller-height-content ">
+            <div class="row info-panel">
+                <div class="col-sm"><img src="img/enduro-g74c77a786_640.jpg" alt="" class="info-panel-photo">
+                    <div class="info-panel-title">
+                        O NÁS
                     </div>
                 </div>
-
-            </div>
-        </div>
-
-        <div class="content">
-            <h1> KDO JSME </h1>
-            <div>
-               <div class="box-first">
-                   <div class="box-first-img">  image </div>
-                   <div class="box-first-text">
-                       <h2> nadpis </h2>
-                       <p>  text etxkl </p>
-                   </div>
-
-
-
-               </div>
-            </div>
-
-        </div>
-    <div class="photo-box">
-        <div class="row">
-            <div class="column">
-                <img src="https://www.w3schools.com//w3images/wedding.jpg" style="width:100%">
-
-            </div>
-
-            <div class="column">
-                <img src="https://www.w3schools.com//w3images/underwater.jpg" style="width:100%">
-
-            </div>
-
-            <div class="column">
-                <img src="https://www.w3schools.com//w3images/wedding.jpg" style="width:100%">
-
-            </div>
-
-            <div class="column">
-                <img src="https://www.w3schools.com//w3images/underwater.jpg" style="width:100%">
-
+                <div class="col-sm"><img src="img/enduro-g74c77a786_640.jpg" alt="" class="info-panel-photo">
+                    <div class="info-panel-title">
+                        AKCE
+                    </div>
+                </div>
+                <div class="col-sm"><img src="img/enduro-g74c77a786_640.jpg" alt="" class="info-panel-photo">
+                    <div class="info-panel-title">
+                        FOTO
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+    <div class="label-fact">
+        !FAKTA!
+    </div>
+</div>
+        <div class="background-row">
+            <div class="content row">
+                <div class="col-sm facts">
+
+                    <div class="riding-girls">
+                    lorem ispum: 10
+                    </div>
+                    <div class="couunt-action">
+                        lorem ispum: 10
+                    </div>
+                    <div class="exp">
+                        lorem ispum: 10
+                    </div>
 
 
-        <div class="content">
-            <h1> KDO JSME </h1>
-            <div>
-                <p> Liquorice tootsie roll danish sweet roll cake. Wafer candy canes sweet roll cheesecake pastry.
-                    Cotton candy tootsie roll pudding biscuit chocolate cheesecake.
-                    Croissant toffee tart liquorice chocolate cake jujubes toffee cotton candy. Bonbon icing donut chupa
-                    chups powder marzipan chupa chups tart pastry.
-                    Sweet tootsie roll icing donut pudding powder sweet roll soufflé toffee. Wafer danish chupa chups
-                    bonbon soufflé bonbon shortbread tiramisu.
-                    Macaroon cotton candy topping chocolate biscuit. Bear claw bonbon bear claw halvah candy canes
-                    pastry.
-                    Chupa chups jelly-o jelly beans gingerbread pudding brownie fruitcake dragée oat cake. Cupcake
-                    marzipan jelly beans donut candy soufflé.
-                    Toffee soufflé candy canes donut pie marzipan carrot cake. Candy canes cake dragée oat cake sugar
-                    plum ice cream tiramisu jelly jelly-o.
-                    Chocolate cake donut oat cake liquorice dragée chocolate bar fruitcake jelly jelly. Topping gummi
-                    bears dragée donut shortbread carrot cake.
-                    Tiramisu pudding icing biscuit oat cake. Fruitcake macaroon sesame snaps icing bonbon fruitcake
-                    biscuit jelly-o sweet roll.
-                    Gummies macaroon bonbon cotton candy carrot cake dessert. Fruitcake chocolate cake muffin cupcake
-                    marzipan sweet chupa chups lollipop.
-                    Sesame snaps candy canes toffee sesame snaps danish candy lollipop soufflé. Chupa chups cookie
-                    shortbread chocolate bar tootsie roll jujubes gummies brownie chocolate.
-                    Jelly beans gummi bears brownie tart dessert cake carrot cake. Sweet candy canes topping chocolate
-                    cheesecake marzipan.
-                    Pudding sweet roll danish bonbon chocolate apple pie pie. Fruitcake brownie chocolate cake dragée
-                    caramels candy canes. Sweet carrot cake chupa chups tart cheesecake. Cotton candy donut halvah
-                    sesame snaps jelly pudding dessert. Gummi bears apple pie biscuit lemon drops apple pie carrot cake
-                    ice cream carrot cake sesame snaps. Pudding oat cake chocolate cake brownie biscuit chupa chups.
-                    Candy canes jelly beans donut brownie oat cake sugar plum. Apple pie jelly cotton candy soufflé
-                    gummi bears. Candy jelly-o gingerbread cake chocolate icing macaroon sweet roll. Tart jelly jelly
-                    beans tart oat cake halvah carrot cake. Donut cookie bonbon shortbread caramels topping pie. Dragée
-                    jujubes dragée liquorice gummies bear claw sugar plum croissant. Sesame snaps lollipop pudding gummi
-                    bears cotton candy carrot cake candy. Sesame snaps dragée halvah dragée chocolate cake carrot cake
-                    jelly-o.</p>
+                </div>
             </div>
-
         </div>
-
-
 </body>
 
 </html>
