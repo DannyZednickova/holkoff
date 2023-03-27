36c7d0987e8c743b27d4a872abcdf887811e747d
Daniela Zedníčková <76813964+DannyZednickova@users.noreply.github.com>
2023-03-27 20:46:52 +0200

    loafing to web

--- index.php
+++ index.php
@@ -1,91 +1,329 @@
 <!DOCTYPE html>
-<html class="background-main">
+<html class="top-menu-full" lang="Utf-8">
 <head>
     <title>
-        Muj domaci ukol </title>
+        Holkoff.cz  </title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/styl.css">
     <link rel="stylesheet" href="assets/icons.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
 </head>
 
 <body>
+<div class="top-intro" style="background-image: url('img/wpbackground.jpg')">
+</div>
 
-<div class="card text-center background-main">
+<img class="logo-img" src="img/holkoff-nobackgr.png" alt="">
+
+<div class="card text-center top-menu-full">
     <div class="card-header ">
         <ul class="nav nav-tabs card-header-tabs ">
-            <li class="nav-item background-main ">
-                <a class="nav-link" href="#">O nás</a>
+            <li class="nav-item ">
+                <a class="nav-link" href="#whoweare">O nás</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Kalendář akcí </a>
+                <a class="nav-link" href="#calendar">Kalendář akcí </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#"> Fotogalerie </a>
+                <a class="nav-link" href="#photos"> Fotogalerie </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#"> Kde nás najdete </a>
+                <a class="nav-link" href="#footer"> Kde nás najdete </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">MERCH</a>
+                <a class="nav-link" href="#" style="color: grey; box-shadow: none;">MERCH</a>
             </li>
         </ul>
     </div>
-    <div class="row">
-        <div class="col-6 move-top-col-6-up">
-            <div class="alert alert-danger alert-transtion" role="alert">
+</div>
 
-                <h3> HOLKOFF 1.4.-3.4. </h3>
-
-            </div>
-        </div>
-        <div class="col-6 move-top-col-6-up">
-            <img class="logo-img" src="img/holkoff-nobackgr.png" alt="">
-        </div>
-
-        <div class="content smaller-height-content ">
-            <div class="row info-panel">
-                <div class="col-sm"><img src="img/enduro-g74c77a786_640.jpg" alt="" class="info-panel-photo">
-                    <div class="info-panel-title">
-                        O NÁS
-                    </div>
-                </div>
-                <div class="col-sm"><img src="img/enduro-g74c77a786_640.jpg" alt="" class="info-panel-photo">
-                    <div class="info-panel-title">
-                        AKCE
-                    </div>
-                </div>
-                <div class="col-sm"><img src="img/enduro-g74c77a786_640.jpg" alt="" class="info-panel-photo">
-                    <div class="info-panel-title">
-                        FOTO
-                    </div>
-                </div>
-            </div>
+<!-- RESPONSIVE - HAMBURDER SMALL DEVICE -->
+<div class="pos-f-t">
+    <div class="collapse" id="navbarToggleExternalContent">
+        <div class=" p-4">
+            <h5 class="text-white h4">Collapsed content</h5>
+            <span class="text-muted">Toggleable via the navbar brand.</span>
         </div>
     </div>
-    <div class="label-fact">
-        !FAKTA!
+    <nav class="navbar navbar-dark bg-dark">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </nav>
+</div>
+
+<div class="label-fact">
+    !FAKTA!
+</div>
+<div class="alert alert-danger alert-transtion" role="alert">
+
+    <h3> HOLKOFF AKCE  </h3>
+    <span> 7.4.-9.4.2023 </span>
+
+</div>
+
+<div class="background-row">
+    <div class="content row">
+        <div class="col-sm facts">
+
+            <div class="facts-item fact-girls">
+                <div class="fact-number">
+                    400+
+                </div>
+                <div class="fact-note">
+                    Holek na motorce
+                </div>
+            </div>
+            <div class="facts-item facts-actions">
+                <div class="fact-number">
+                    3
+                </div>
+                <div class="fact-note">
+                    Akce za námi
+                </div>
+            </div>
+            <div class="facts-item facts-ext">
+                <div class="fact-number">
+                    100%
+                </div>
+                <div class="fact-note">
+                    Zážitků!
+                </div>
+            </div>
+
+
+        </div>
     </div>
 </div>
-        <div class="background-row">
-            <div class="content row">
-                <div class="col-sm facts">
-
-                    <div class="riding-girls">
-                    lorem ispum: 10
-                    </div>
-                    <div class="couunt-action">
-                        lorem ispum: 10
-                    </div>
-                    <div class="exp">
-                        lorem ispum: 10
-                    </div>
+<!--WHO WE ARE  -->
 
 
-                </div>
-            </div>
+<!-- WHO WE ARE  -->
+
+<div class="content">
+    <div class="who-we-are" id="whoweare">
+        <div class="photo-who-we-are">
+            <img src="img/extreme.jpg" alt="">
         </div>
-</body>
+        <div class="who-we-are-textarea">
+            <h1 style="    padding: 1em;">WHO WE ARE </h1>
+            <ul>
+                <li>
+                    Položka
+                </li>
+                <li>
+                    Položka
+                </li>
+                <li>
+                    Položka
+                </li>
+                <li>
+                    Položka
+                </li>
 
+            </ul>
+        </div>
+
+
+    </div>
+    <div class="who-we-are-label">
+        Who We Are
+    </div>
+</div>
+
+<div class="content">
+    <div class="calendar" id="calendar">
+        <img src="img/velikonoce.jpg" alt="" class="tmp-easter">
+    </div>
+
+</div>
+
+<!-- WHO WE ARE  -->
+
+
+
+<!-- Gallery -->
+<div class="content">
+
+    <h1 style = "    margin-top: 1em;
+    margin-bottom: 1em;">FOTOGALERIE</h1>
+</div>
+
+    <div class="row galery" id="photos">
+        <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+            <img
+                    src="img/enduro.jpg"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Boat on Calm Water"
+            />
+
+            <img
+                    src="img/extreme.jpg"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Wintry Mountain Landscape"
+            />
+        </div>
+
+        <div class="col-lg-4 mb-4 mb-lg-0">
+            <img
+                    src="img/extreme.jpg"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Mountains in the Clouds"
+            />
+
+            <img
+                    src="img/enduro.jpg"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Boat on Calm Water"
+            />
+        </div>
+
+        <div class="col-lg-4 mb-4 mb-lg-0">
+            <img
+                    src="img/enduro.jpg"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Waves at Sea"
+            />
+
+            <img
+                    src="img/extreme.jpg"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Yosemite National Park"
+            />
+        </div>
+    </div>
+
+<!-- Gallery -->
+
+
+<!-- SPONZORS - TO DO  -->
+
+
+<!-- LEADERS - TO DO -->
+
+
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted" id="footer">
+    <!-- Section: Social media -->
+    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <!-- Left -->
+        <div class="me-5 d-none d-lg-block">
+            <span>JAK SE S NÁMI SPOJIT:</span>
+        </div>
+        <!-- Left -->
+
+        <!-- Right -->
+        <div>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-twitter"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-google"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-instagram"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-linkedin"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-github"></i>
+            </a>
+        </div>
+        <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+
+    <!-- Section: Links  -->
+    <section class="">
+        <div class="container text-center text-md-start mt-5">
+            <!-- Grid row -->
+            <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <!-- Content -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        <i class="fas fa-gem me-3"></i>Company name
+                    </h6>
+                    <p>
+                        Here you can use rows and columns to organize your footer content. Lorem ipsum
+                        dolor sit amet, consectetur adipisicing elit.
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Products
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-reset">Angular</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">React</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Vue</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Laravel</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Useful links
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-reset">Pricing</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Settings</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Orders</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Help</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+                    <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+                    <p>
+                        <i class="fas fa-envelope me-3"></i>
+                        info@example.com
+                    </p>
+                    <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+                    <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+        </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+        © 2023 Copyright: Daniela Zedníčková
+    </div>
+    <!-- Copyright -->
+</footer>
+<!-- Footer -->
+</body>
 </html>
